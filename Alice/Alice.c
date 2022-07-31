fec3956f233b3657c1d47db5977f2431e518b2fa
threessz <108932572+threessz@users.noreply.github.com>
2022-07-31 20:08:46 +0800

    Add files via upload

---
+++ Alice/Alice.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main(void)
+//{
+//	int feet, fathoms;
+//
+//	fathoms = 2;
+//	feet = 6 * fathoms;
+//	printf("there are %d feet in %d fathoms!\n", feet, fathoms);
+//	printf("yes,I said %d feet!\n", 6 * fathoms);
+//
+//	return 0;
+//}
+
+//void butler(void);
+//int main(void)
+//{
+//	printf("i will summon the butler function.\n");
+//	butler();
+//	printf("yes,bring me some tea and writeable DVDs.\n");
+//
+//	return 0;
+//}
+//void butler(void)
+//{
+//	printf("You rang,sir?\n");
+//}
+
+//int main(void)
+//{
+//	int n, n2, n3;
+//	n = 5;
+//	n2 = n * n;
+//	n3 = n2 * n2;
+//	printf("n=%d,n squared=%d,n cubed=%d\n", n, n2, n3);
+//
+//	return 0;
+//}
+//
+//#include <stdio.h>
+//int main(void)
+//{
+//	int s;
+//	s = 56;
+//	printf("there are %d weeks a year.\n", s);
+//	return 0;
+//}
+//
+//a.Baa Baa Black Sheephave you any wool
+//b.Begone!
+//creature of lard!
+//c.What?
+//No/Nfish?\n  
+//d.2+2=4
+//
+//纸上得来终觉浅，绝知此事要躬行
+//
+//#include <stdio.h>
+//int main()
+//{
+	/*printf("hu tao");*/
+	//printf("hu\n");
+	//printf("tao");
+	/*printf("hu");
+	printf("tao");*/
+
+	/*printf("My name is Hu Tao.\n");
+	printf("I live in China.");
+
+
+
+	return 0;
+}*/
+
+
+int main(void)
+{
+	int a, b;
+	a= 18;
+	b = a * 365;
+	printf("I have lived in this world for %d years,which is %d days.\n", a, b);
+	return 0;
+}
